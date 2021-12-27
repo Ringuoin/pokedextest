763a53a1cad830f6e8e15fa32bf91fb91586cd7c
Ringuoin <cedric.rous@hotmail.fr>
2021-12-27 14:16:11 +0100

    commit fait

---
+++ pokedex/src/components/layout/Barre.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Barre extends Component {
+    render() {
+        return (
+            <div>
+                <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'></nav>
+                heho
+            </div>
+        )
+        }
+}
